856e88cb086a0fabbae842087cf0c6409d17838b
Alison Machado <alison.sm92@gmail.com>
2026-04-06 18:45:22 -0300

    fix: coverage command on CI

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
     testEnvironment: 'node',
+    testMatch: ['**/test/**/*.spec.js'],
     collectCoverageFrom: [
         'lib/**/*.js',
         '!**/node_modules/**',
     ],
     coverageReporters: ['lcov', 'text', 'text-lcov'],
+    forceExit: true,
+    detectOpenHandles: true,
 };
